08a83a35aeafb99ff1b9adab707e79ca383c761f
Ghostlander <ghostlander@gmail.com>
2014-01-15 21:47:41 +0200

    Fix Memory Leaks Due to Missing ECDSA_SIG_free()

--- src/key.cpp
+++ src/key.cpp
@@ -311,8 +311,10 @@ bool CKey::SignCompact(uint256 hash, std::vector<unsigned char>& vchSig)
                 }
         }
 
-        if (nRecId == -1)
+        if (nRecId == -1) {
+            ECDSA_SIG_free(sig);
             throw key_error("CKey::SignCompact() : unable to construct recoverable key");
+        }
 
         vchSig[0] = nRecId+27+(fCompressedPubKey ? 4 : 0);
         BN_bn2bin(sig->r,&vchSig[33-(nBitsR+7)/8]);
@@ -351,6 +353,7 @@ bool CKey::SetCompactSignature(uint256 hash, const std::vector<unsigned char>& v
         ECDSA_SIG_free(sig);
         return true;
     }
+    ECDSA_SIG_free(sig);
     return false;
 }
 
